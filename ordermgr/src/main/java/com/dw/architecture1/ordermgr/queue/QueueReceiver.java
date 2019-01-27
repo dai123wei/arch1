65d8fc0b2f4da6e9c46bcd8a68f0008f7602a2a5
dai123wei <13260982820@163.com>
2019-01-27 15:51:17 +0800

    add mutil thread

--- ordermgr/src/main/java/com/dw/architecture1/ordermgr/queue/QueueReceiver.java
+++ ordermgr/src/main/java/com/dw/architecture1/ordermgr/queue/QueueReceiver.java
@@ -3,7 +3,6 @@ package com.dw.architecture1.ordermgr.queue;
 import com.dw.architecture1.cartmgr.service.ICartService;
 import com.dw.architecture1.cartmgr.vo.CartModel;
 import com.dw.architecture1.cartmgr.vo.CartQueryModel;
-import com.dw.architecture1.ordermgr.dao.OrderDAO;
 import com.dw.architecture1.ordermgr.service.IOrderDetailService;
 import com.dw.architecture1.ordermgr.service.IOrderService;
 import com.dw.architecture1.ordermgr.vo.OrderDetailModel;
@@ -14,6 +13,7 @@ import com.dw.architecture1.storemgr.vo.StoreModel;
 import com.dw.common.pageutil.Page;
 import com.dw.common.util.format.DateFormatHelper;
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.ActiveMQMessageConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
@@ -39,72 +39,11 @@ public class QueueReceiver implements ServletContextListener{
         try {
             ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(
                     "tcp://192.168.0.30:61616");
-            connection = cf.createConnection();
-            connection.start();
 
-            session = connection.createSession(Boolean.TRUE,
-                    Session.AUTO_ACKNOWLEDGE);
-            Destination destination = session.createQueue("order-queue");
-            MessageConsumer consumer = session.createConsumer(destination);
-
-            consumer.setMessageListener(new MessageListener() {
-                public void onMessage(Message message) {
-                    try {
-                        TextMessage textMessage = (TextMessage) message;
-                        int customerId = Integer.parseInt(textMessage.getText());
-                        session.commit();
-                        CartQueryModel cqm = new CartQueryModel();
-                        cqm.getPage().setPageShow(1000);
-                        cqm.setCustomerUuid(customerId);
-
-                        Page<CartModel> page = cartService.getByConditionPage(cqm);
-
-                        //2
-                        float totalMoney = 0.0f;
-                        for(CartModel cm : page.getResult()){
-                            totalMoney += 10;
-                        }
-                        OrderModel om = new OrderModel();
-                        om.setCustomerUuid(customerId);
-                        om.setOrderTime(DateFormatHelper.long2str(System.currentTimeMillis()));
-                        om.setSaveMoney(0.0F);
-                        om.setTotalMoney(totalMoney);
-                        om.setState(1);
-
-                        orderService.create(om);
-
-                        OrderQueryModel oqm = new OrderQueryModel();
-                        oqm.setOrderTime(om.getOrderTime());
-
-                        Page<OrderModel> omPage = orderService.getByConditionPage(oqm);
-                        om = omPage.getResult().get(0);
-
-                        //3
-                        for (CartModel cm : page.getResult()){
-                            OrderDetailModel odm = new OrderDetailModel();
-                            odm.setGoodsUuid(cm.getGoodsUuid());
-                            odm.setOrderNum(cm.getBuyNum());
-                            odm.setPrice(10.0F);
-                            odm.setMoney(odm.getPrice() * odm.getOrderNum());
-                            odm.setSaveMoney(0.0F);
-                            odm.setOrderUuid(om.getUuid());
-
-                            orderDetailService.create(odm);
-
-                            //4
-                            StoreModel sm = storeService.getByGoodsUuid(cm.getGoodsUuid());
-                            sm.setStoreNum(sm.getStoreNum() - odm.getOrderNum());
-                            storeService.update(sm);
-
-                            //5
-                            cartService.delete(cm.getUuid());
-                        }
-                    } catch (JMSException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        } catch (JMSException e) {
+            for(int i=0;i<1;i++){
+                new MyThread(cartService, orderService, orderDetailService, storeService, cf).start();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -117,6 +56,106 @@ public class QueueReceiver implements ServletContextListener{
 
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
 
+    }
+}
+
+class MyThread extends Thread {
+    private ICartService ics = null;
+    private IOrderService ios = null;
+    private IOrderDetailService iods = null;
+    private IStoreService iss = null;
+    private ActiveMQConnectionFactory cf = null;
+    public MyThread(ICartService ics, IOrderService ios, IOrderDetailService iods,
+               IStoreService iss, ActiveMQConnectionFactory cf) {
+        super();
+        this.ics = ics;
+        this.ios = ios;
+        this.iods = iods;
+        this.iss = iss;
+        this.cf = cf;
+    }
+
+    public void run(){
+        Connection connection = null;
+        final Session session;
+        try {
+            connection = cf.createConnection();
+            connection.start();
+
+            session = connection.createSession(Boolean.TRUE,
+                    Session.AUTO_ACKNOWLEDGE);
+            Destination destination = session.createQueue("order-queue");
+
+            ActiveMQMessageConsumer consumer = (ActiveMQMessageConsumer) session
+                    .createConsumer(destination);
+
+            consumer.setMessageListener(new MessageListener() {
+                public void onMessage(Message msg) {
+                    try {
+                        TextMessage txtMsg = (TextMessage) msg;
+                        int customerUuid = Integer.parseInt(txtMsg.getText());
+
+                        session.commit();
+
+                        CartQueryModel cqm = new CartQueryModel();
+                        cqm.getPage().setPageShow(1000);
+                        cqm.setCustomerUuid(customerUuid);
+
+                        Page<CartModel> page = ics.getByConditionPage(cqm);
+                        // 2:
+                        float totalMoney = 0.0f;
+                        for (CartModel cm : page.getResult()) {
+                            totalMoney += 10;
+                        }
+
+                        OrderModel order = new OrderModel();
+                        order.setCustomerUuid(customerUuid);
+                        order.setOrderTime(DateFormatHelper.long2str(System
+                                .currentTimeMillis()));
+                        order.setSaveMoney(0.0F);
+                        order.setTotalMoney(totalMoney);
+                        order.setState(1);
+
+                        ios.create(order);
+
+                        OrderQueryModel oqm = new OrderQueryModel();
+                        oqm.setOrderTime(order.getOrderTime());
+
+                        Page<OrderModel> orderPage = ios
+                                .getByConditionPage(oqm);
+                        order = orderPage.getResult().get(0);
+
+                        // 3:
+                        for (CartModel cm : page.getResult()) {
+                            OrderDetailModel odm = new OrderDetailModel();
+                            odm.setGoodsUuid(cm.getGoodsUuid());
+                            odm.setOrderNum(cm.getBuyNum());
+                            odm.setPrice(10.0f);
+                            odm.setMoney(odm.getPrice() * odm.getOrderNum());
+                            odm.setSaveMoney(0.0f);
+                            odm.setOrderUuid(order.getUuid());
+
+                            iods.create(odm);
+                            // 4:
+                            synchronized (iss) {
+                                StoreModel storeModel = iss.getByGoodsUuid(cm
+                                        .getGoodsUuid());
+                                storeModel.setStoreNum(storeModel.getStoreNum()
+                                        - odm.getOrderNum());
+                                iss.update(storeModel);
+                            }
+                            // 5:
+    						ics.delete(cm.getUuid());
+                        }
+                    } catch (Exception err) {
+                        err.printStackTrace();
+                    }
+                }
+            });
+
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
     }
 }
 
